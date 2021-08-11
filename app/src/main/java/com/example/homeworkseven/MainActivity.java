e726e988e44bab7892d625694b59d6c38dbe82b8
abricos <fruts@nadereve.ru>
2021-08-11 23:22:37 +0300

    создание actionBar

--- app/src/main/java/com/example/homeworkseven/MainActivity.java
+++ app/src/main/java/com/example/homeworkseven/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.homeworkseven;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +20,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         getSupportFragmentManager().
                 beginTransaction().
                 replace(R.id.notes_container, com.example.homeworkseven.NotesFragment.newInstance()).
                 commit();
+
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             getSupportFragmentManager().
                     beginTransaction().
                     replace(R.id.description_container, FragmentDescription.newInstance(new Notes("text","text"))).
                     commit();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
         }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+switch (item.getItemId()){
+    case R.id.action_setting:
+        Toast toast1 = Toast.makeText(getApplicationContext(),
+                "Запуск настроек", Toast.LENGTH_SHORT);
+        toast1.show();
+        break;
+
+    case R.id.action_about_application:
+        Toast toast2 = Toast.makeText(getApplicationContext(),
+                "Информация о приложении", Toast.LENGTH_SHORT);
+        toast2.show();
+        break;
+
+}
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
